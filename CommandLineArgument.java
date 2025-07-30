5c3d0642018a4b0529a2c464bcf0b6b4d566136e
yogita0311 <yogitaraut0311@gmail.com>
2025-07-30 14:37:41 -0700

    Add CommandLineArgument.java

---
+++ CommandLineArgument.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class CommandLineArgument {
+public static void main(String[] ram){
+System.out.println(ram.length);
+System.out.println("You have enter name is:");
+for(int i=0; i<ram.length; i++){
+System.out.println(ram[i]);
+}
+}
+}
