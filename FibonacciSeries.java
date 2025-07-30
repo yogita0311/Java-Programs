bb9b0e7c0f57f4e0c25fa47df7068dff402677f9
yogita0311 <yogitaraut0311@gmail.com>
2025-07-30 14:21:40 -0700

    Add FibonacciSeries.java file

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class FibonacciSeries {
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int term, a=0, b=1, c;
+System.out.println("Enter term:");
+term=sc.nextInt();
+for(int i=1; i<=term; i++){
+System.out.print(a+ " ");
+c=a+b;
+a=b;
+b=c;
+}
+}
+}
